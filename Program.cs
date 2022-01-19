6f51a02a25330058031e184e0bf0833545ffa7e3
FabiKuri <fabien.k.main@gmail.com>
2022-01-20 00:47:42 +0900

    練習問題３-１９

--- Program.cs
+++ Program.cs
@@ -4,23 +4,13 @@ namespace C_sharp
 {
     class Program
     {
+        static void Thankyou()
+        {
+            Console.WriteLine("ありがとうございました");
+        }
         static void Main(string[] args)
         {
-            //変数をまとめて初期化できる
-            float[] weights = {41.2f, 42.5f, 44.9f, 43.2f, 45.1f, 43.2f, 42.7f, 41.5f, 41.4f, 41.9f};
-
-            
-            
-            for (int i = 0;i < weights.Length; i++)
-            {
-                if (weights[i] <= 43.0)
-                {
-                    Console.WriteLine(weights[i]);
-                }
-            }
-            
-            // float average = sum / weights.Length;
-            // Console.WriteLine("平均値は" + average + "です");
+            Thankyou();
         }
     }
 }
