79612b05b970c71f086bc7c1e46dcfd25116fbe9
FabiKuri <fabien.k.main@gmail.com>
2022-01-20 02:56:49 +0900

    練習問題3−21

--- Program.cs
+++ Program.cs
@@ -4,40 +4,46 @@ namespace C_sharp
 {
     class Program
     {
-        // static void Shop(string itemName)
-        // {
-        //     Console.WriteLine("いらっしゃいませ");
-
-        //     if (itemName == "薬草")
-        //     {
-        //         Console.WriteLine(itemName + "は100円です");
-        //     }
-        //     else if (itemName == "棍棒")
-        //     {
-        //         Console.WriteLine(itemName + "は1500円です");
-        //     }
-        //     else 
-        //     {
-        //         Console.WriteLine("売り切れです");
-        //     }
-        // }
-
-        static void ShowEvenOrOdd(int num)
+        static int Shop(string itemName)
         {
-            if (num % 2 == 0)
+            Console.WriteLine("いらっしゃいませ");
+            int price = 0;
+
+            if (itemName == "薬草")
             {
-                Console.WriteLine("偶数です");
+                Console.WriteLine(itemName + "は100円です");
+                price = 100;
             }
-            else
+            else if (itemName == "万能薬")
             {
-                Console.WriteLine("奇数です");
+                Console.WriteLine(itemName + "は500円です");
+                price = 500;
             }
+            else if (itemName == "棍棒")
+            {
+                Console.WriteLine(itemName + "は1500円です");
+                price = 1500;
+            }
+            else 
+            {
+                Console.WriteLine("売り切れです");
+                price = 0;
+            }
+
+            return price;
         }
         static void Main(string[] args)
         {
+            int money = 2500;
+            Console.WriteLine("所持金は" + money + "円です");
+            int price = Shop("薬草");
+            money -= price;
+            Console.WriteLine("所持金は" + money + "円です");
+            
+            price = Shop("万能薬");
+            money -= price;
+            Console.WriteLine("所持金は" + money + "円です");
 
-            ShowEvenOrOdd(4);
-            // Shop("薬草");
         }
     }
 }
