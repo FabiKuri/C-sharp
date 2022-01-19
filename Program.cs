555ac08793695b01b2f74cb3580d2305bf8a7465
FabiKuri <fabien.k.main@gmail.com>
2022-01-20 00:15:57 +0900

    P94：サンプル17

--- Program.cs
+++ Program.cs
@@ -9,12 +9,15 @@ namespace C_sharp
             //変数をまとめて初期化できる
             float[] weights = {41.2f, 42.5f, 44.9f, 43.2f, 45.1f, 43.2f, 42.7f, 41.5f, 41.4f, 41.9f};
 
-            //
-            foreach (float w in weights)
+            float sum = 0.0f;
+            
+            for (int i = 0;i < weights.Length; i++)
             {
-                Console.WriteLine(w);
+                sum += weights[i];
             }
             
+            float average = sum / weights.Length;
+            Console.WriteLine("平均値は" + average + "です");
         }
     }
 }
