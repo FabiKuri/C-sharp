1ef8df704adfcc8347cb7f95162037e79e32c8c3
FabiKuri <fabien.k.main@gmail.com>
2022-01-19 22:27:39 +0900

    P93：foreach文

--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ namespace C_sharp
             float[] weights = {41.2f, 42.5f, 44.9f, 43.2f, 45.1f, 43.2f, 42.7f, 41.5f, 41.4f, 41.9f};
 
             //
-            for (int i = 0;i < weights.Length; i++)
+            foreach (float w in weights)
             {
-                Console.WriteLine(weights[i]);
+                Console.WriteLine(w);
             }
             
         }
