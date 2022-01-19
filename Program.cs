cb85c081df6247ecdf47ceb58c24b5716551b902
FabiKuri <fabien.k.main@gmail.com>
2022-01-19 18:05:32 +0900

    練習問題3-8

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ namespace C_sharp
     {
         static void Main(string[] args)
         {
-            int player1PosX = 2;
+            int player1PosX = 1;
             int player2PosX = 6;
 
-            if (player1PosX == 2 && player2PosX ==6)
+            if (player1PosX == 2 || player2PosX ==6)
             {
                 Console.WriteLine("仕掛け解除");
             }
