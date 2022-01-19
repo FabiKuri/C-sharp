a6d660c5802c37ba92ddfcb60cfe39352db71a7d
FabiKuri <fabien.k.main@gmail.com>
2022-01-20 00:30:10 +0900

    練習問題３-１８

--- Program.cs
+++ Program.cs
@@ -9,15 +9,18 @@ namespace C_sharp
             //変数をまとめて初期化できる
             float[] weights = {41.2f, 42.5f, 44.9f, 43.2f, 45.1f, 43.2f, 42.7f, 41.5f, 41.4f, 41.9f};
 
-            float sum = 0.0f;
+            
             
             for (int i = 0;i < weights.Length; i++)
             {
-                sum += weights[i];
+                if (weights[i] <= 43.0)
+                {
+                    Console.WriteLine(weights[i]);
+                }
             }
             
-            float average = sum / weights.Length;
-            Console.WriteLine("平均値は" + average + "です");
+            // float average = sum / weights.Length;
+            // Console.WriteLine("平均値は" + average + "です");
         }
     }
 }
