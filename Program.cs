bc49e04dc0a8ff016f51573a39d26cd2ebcef3f1
FabiKuri <fabien.k.main@gmail.com>
2022-01-20 01:52:15 +0900

    103P：サンプル19

--- Program.cs
+++ Program.cs
@@ -4,13 +4,26 @@ namespace C_sharp
 {
     class Program
     {
-        static void Thankyou()
+        static void Shop(string itemName)
         {
-            Console.WriteLine("ありがとうございました");
+            Console.WriteLine("いらっしゃいませ");
+
+            if (itemName == "薬草")
+            {
+                Console.WriteLine(itemName + "は100円です");
+            }
+            else if (itemName == "棍棒")
+            {
+                Console.WriteLine(itemName + "は1500円です");
+            }
+            else 
+            {
+                Console.WriteLine("売り切れです");
+            }
         }
         static void Main(string[] args)
         {
-            Thankyou();
+            Shop("薬草");
         }
     }
 }
